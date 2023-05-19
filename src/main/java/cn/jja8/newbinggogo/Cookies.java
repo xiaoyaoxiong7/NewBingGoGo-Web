7e4b14417959b17be901f889d813c9feea4f101c
xiaoyaoxiong7 <133345431+xiaoyaoxiong7@users.noreply.github.com>
2023-05-19 14:42:31 +0800

    Update Cookies.java

--- src/main/java/cn/jja8/newbinggogo/Cookies.java
+++ src/main/java/cn/jja8/newbinggogo/Cookies.java
@@ -3,5 +3,5 @@ package cn.jja8.newbinggogo;
 import cn.jja8.config.tool.Conf;
 
 public class Cookies {
-    @Conf static String[] cookies = {};
+    @Conf static String[] cookies = {ANON=A=17333C5929E2F6E8404EB8DDFFFFFFFF&E=1c35&W=3; NAP=V=1.9&E=1bdb&C=DwOS_Wb9EYRHBSu9nQd6aWXZehRdS4TfGrwosXM29ikYL8Ht2FpYdw&W=3; MUID=2E442C776AA367151C8D3D066EA36130};
 }
